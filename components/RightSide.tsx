01e0c23e41c6eb2266a585db36cc5a8acdfd438e
profmoriartey <kaito9814@gmail.com>
2023-09-05 20:20:18 +0300

    Correct email adress

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const RightSide = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-end gap-8 text-textLight">
-      <a href="mailto:aaajh98@gmail.com">
+      <a href="mailto:ahmed.a.alhusaini@gmail.com">
         <p className="text-sm rotate-90 w-72 tracking-wide  text-textDark">
           ahmed.a.alhusaini@gmail.com
         </p>
