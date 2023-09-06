e12db85276a9455da5fe9766062860d52a8d3d1a
profmoriartey <kaito9814@gmail.com>
2023-09-06 23:13:18 +0300

    Correct cv link on mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -212,7 +212,7 @@ const Navbar = () => {
                     </motion.li>
                   </Link>
                 </ul>
-                <a href="/CV-Ahmed-Alhusaini.pdf" target="_blank">
+                <a href="/ahmed-alhusaini-CV.pdf" target="_blank">
                   <motion.button
                     initial={{ y: -10, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
