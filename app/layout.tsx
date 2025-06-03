85aac61bf3c1f965fb09d18dc38f5094ffc335ff
Ahmed Alhusaini <61118848+ProfMoriartey@users.noreply.github.com>
2025-06-03 12:28:25 +0300

    Update layout.tsx add ahref script for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><script src="https://analytics.ahrefs.com/analytics.js" data-key="9W3MfDgbQ51Z2iyZSBW2cg" async></script></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
